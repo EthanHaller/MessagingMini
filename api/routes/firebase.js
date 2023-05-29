4a9de381bde978adcd97712379fa08a02608e32c
EthanHaller <ttk4ey@virginia.edu>
2023-05-29 10:13:53 -0400

    project setup

---
+++ api/routes/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app"; 
+import { getFirestore } from "firebase/firestore";
+
+import serviceAccount from "../permissions.json";
+
+const app = initializeApp(serviceAccount);
+const db = getFirestore(app);
+
+export default db;
